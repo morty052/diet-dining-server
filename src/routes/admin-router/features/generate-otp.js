1818433a6319261fec021fb2980824b05154597c
morty052 <anomaly619052@gmail.com>
2024-02-15 19:13:47 +0100

    d

--- src/routes/admin-router/features/generate-otp.js
+++ src/routes/admin-router/features/generate-otp.js
@@ -3,10 +3,10 @@ import sanityClient from "../../../lib/sanityClient.js";
 export const generate_otp = async (admin_id) => {
   try {
     const otp = Math.floor(10000 + Math.random() * 90000);
-    // await sanityClient
-    //   .patch(admin_id)
-    //   .set({ admin_active_otp: `${otp}` })
-    //   .commit();
+    await sanityClient
+      .patch(admin_id)
+      .set({ admin_active_otp: `${otp}` })
+      .commit();
     return otp;
   } catch (error) {
     console.log(error);
